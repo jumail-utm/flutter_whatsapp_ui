09bb5dadfa7b185f95f068eb7cc82663afb7e6cb
Jumail <jumail@utm.my>
2020-03-02 11:29:29 +0800

    add TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -14,15 +14,45 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(icon: Icon(Icons.menu), onPressed: _doNothing),
-        title: Text('Whatsapp'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.search), onPressed: _doNothing),
-          IconButton(icon: Icon(Icons.message), onPressed: _doNothing),
-          IconButton(icon: Icon(Icons.more_vert), onPressed: _doNothing),
-        ],
+    return DefaultTabController(
+      length: 3,
+      initialIndex: 1,
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: _doNothing,
+          ),
+          title: Text('Whatsapp'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: _doNothing,
+            ),
+            IconButton(
+              icon: Icon(Icons.message),
+              onPressed: _doNothing,
+            ),
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: _doNothing,
+            ),
+          ],
+          bottom: TabBar(tabs: [
+            Tab(
+              text: 'CALLS',
+              icon: Icon(Icons.call),
+            ),
+            Tab(
+              text: 'CHATS',
+              icon: Icon(Icons.chat),
+            ),
+            Tab(
+              text: 'CONTACTS',
+              icon: Icon(Icons.contacts),
+            ),
+          ]),
+        ),
       ),
     );
   }
