d679b34fb4aefc507f26d73ba8c87394d36821bd
Jumail <jumail@utm.my>
2020-03-02 11:02:30 +0800

    add appBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main()=>runApp(FlutterLogo(size:double.infinity));
+void main() => runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Whatsapp Clone',
+        theme: ThemeData(primarySwatch: Colors.teal),
+        home: Home(),
+      ),
+    );
 
+class Home extends StatelessWidget {
+  void _doNothing() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.menu), onPressed: _doNothing),
+        title: Text('Whatsapp'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.search), onPressed: _doNothing),
+          IconButton(icon: Icon(Icons.message), onPressed: _doNothing),
+          IconButton(icon: Icon(Icons.more_vert), onPressed: _doNothing),
+        ],
+      ),
+    );
+  }
+}
